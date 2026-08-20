25dab5d1887266365ab303e20bcfc3c357363292
wilson <huguowei007@hotmail.com>
2026-08-20 14:33:36 +0900

    Add files via upload

---
+++ src/components/first.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <h1 :class="'title'">Make me red</h1>
+  <button @click="increment">My Count is: {{ count }}</button>
+
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App1 from './components/first.vue'
+//import App1 from './components/HelloWorld.vue'
+
+createApp(App1).mount('#app')
